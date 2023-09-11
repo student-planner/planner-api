fd169236c8bc90b8d41ceee06c60f5cdf0cb2191
Pavel Markeloff <pmarkelo77@gmail.com>
2023-09-11 23:03:06 +0300

    Добавил атрибуты для состояния модели данных при обновлении задачи

--- Planner.Contracts/Goal/GoalUpdateDto.cs
+++ Planner.Contracts/Goal/GoalUpdateDto.cs
@@ -1,4 +1,6 @@
-﻿namespace Planner.Contracts.Goal;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Planner.Contracts.Goal;
 
 /// <summary>
 /// Модель данных для обновления задачи
@@ -8,10 +10,12 @@ public class GoalUpdateDto
     /// <summary>
     /// Название
     /// </summary>
+    [Required(ErrorMessage = "Не указано название задачи!")]
     public string Name { get; set; } = string.Empty;
 
     /// <summary>
     /// Описание
     /// </summary>
+    [Required(ErrorMessage = "Не указано описание задачи!")]
     public string Description { get; set; } = string.Empty;
 }
