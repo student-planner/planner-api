002cabb41e0092e0dae32d5ed284f2c93834b033
Pavel Markeloff <pmarkelo77@gmail.com>
2023-09-19 14:14:53 +0300

    Model for the algorithm

---
+++ Planner.AlgorithmPriorityGoals/AlgorithmItem.cs
@@ -0,0 +1,34 @@
+﻿namespace Planner.AlgorithmPriorityGoals;
+
+public class AlgorithmItem
+{
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Крайний срок выполнения
+    /// </summary>
+    public DateTime? Deadline { get; set; }
+    
+    /// <summary>
+    /// Трудоёмкость
+    /// </summary>
+    public double? Labor { get; set; }
+    
+    /// <summary>
+    /// Приоритет
+    /// </summary>
+    public int? Priority { get; set; }
+    
+    /// <summary>
+    /// Идентификатор самой приоритетной задачи
+    /// </summary>
+    public Guid DependsPriority { get; set; } = Guid.Empty;
+    
+    /// <summary>
+    /// Коллекция идентификаторов зависимых задач
+    /// </summary>
+    public ICollection<Guid> DependsIds { get; set; } = new List<Guid>();
+}
